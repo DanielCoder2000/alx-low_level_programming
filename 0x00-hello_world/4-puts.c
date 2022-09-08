56e8354175060749cc2cd33fad06bdb139ae2154
Letsatsi Mothemane <mothemaneld@gmail.com>
2022-09-08 13:49:18 -0700

    first C commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/* 
+ *include includes the header
+ * */
+
+int main(){
+/*
+ *main method is where we store our code
+ * */
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+/*
+ * this method is used to include the header
+ * */
+int main(){
+/*
+ *this is where we store our code
+ */
+
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
+}
