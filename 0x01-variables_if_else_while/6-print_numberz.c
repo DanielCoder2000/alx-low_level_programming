eb2b565d3d2af908bb0854c9242f91a244014a1d
Letsatsi Mothemane <mothemaneld@gmail.com>
2022-09-12 13:34:14 -0700

    my if else while commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+/*
+ *Author: Letsatsi Mothemane
+ *
+ *Filename: 6-print_numberz.c 2022/09/12
+ */
+#include<stdio.h>
+/**
+ * main - Prints "all the number of base10
+ * ascendingly" then plints a new line
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int a;
+
+for (a = 0; a < 10; a++)
+{
+putchar(a);
+}
+putchar('\n');
+
+return (0);
+}
