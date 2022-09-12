2ca9f029bae0df99397995fe0bd835c812c26ff3
Letsatsi Mothemane <mothemaneld@gmail.com>
2022-09-12 12:58:24 -0700

    my if else while commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+/*
+ *Author: Letsatsi Mothemane
+ *
+ *filename: 4-print_alphabt.c 2022/09/12
+ */
+#include<stdio.h>
+/**
+ * main - Prints "all letters in lower - case except
+ * for e and q," then prints a new line
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter == 'e' && letter == 'q')
+{
+continue;
+}
+putchar(letter);
+}
+
+return (0);
+}
