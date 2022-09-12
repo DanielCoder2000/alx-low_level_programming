ddc6064657af59890618abf1bfffed3b36248b28
Letsatsi Mothemane <mothemaneld@gmail.com>
2022-09-12 14:25:54 -0700

    my if else while commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+/*
+ *Author: Letsatsi Mothemane
+ *
+ *Filename: 9-print_comb.c 2022/09/12
+ */
+#include<stdio.h>
+/**
+ * main - Prints "numbers of single from 1 to
+ * 9," then prints a new line
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int num;
+
+for (num = 0; num <= 9; num++)
+{
+putchar((num % 10) + '0');
+if (num == 9)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+}
