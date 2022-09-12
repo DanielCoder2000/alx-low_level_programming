a45c238678c9d1489bd91b4acd55ca3d9fda3da8
Letsatsi Mothemane <mothemaneld@gmail.com>
2022-09-12 00:59:47 -0700

    my first commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,8 +6,8 @@
 #include <stdio.h>
 
 /*
- * main-prints the size of bytes
- * Description: using the main function
+ * main - prints the size of bytes
+ * description: using the main function
  * this Prints "the size of various bytes depending on
  * the computer its printed from" then prints the next line
  *
